8d5b71e64df1b0d877946c8957f5c1cb6f071358
el-zacharoo <you@example.com>
2023-03-15 13:44:32 +1300

    updated main file

--- main.go
+++ main.go
@@ -1,45 +1,25 @@
 package main
 
 import (
-	"net/http"
+	// "github.com/endo-checker/common/store"
 
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
-
+	"github.com/endo-checker/common/server"
 	"github.com/endo-checker/patient/handler"
 	pbcnn "github.com/endo-checker/patient/internal/gen/patient/v1/patientv1connect"
 	"github.com/endo-checker/patient/store"
-	"github.com/rs/cors"
+	"github.com/joho/godotenv"
 )
 
-const port = "localhost:8080"
-
 func main() {
-	s := store.Connect()
+	port := "localhost:8080"
+	godotenv.Load()
+	// uri := os.Getenv("mongouri")
 
+	s := store.Connect()
 	svc := &handler.PatientServer{
 		Store: s,
 	}
 
-	c := setCORS()
-
-	mux := http.NewServeMux()
 	path, h := pbcnn.NewPatientServiceHandler(svc)
-	mux.Handle(path, h)
-	handler := c.Handler(mux)
-
-	http.ListenAndServe(
-		port,
-		h2c.NewHandler(handler, &http2.Server{}),
-	)
-}
-
-func setCORS() *cors.Cors {
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedHeaders:   []string{"Access-Control-Allow-Origin", "Content-Type"},
-		AllowedMethods:   []string{"POST", "OPTIONS"},
-		AllowCredentials: true,
-	})
-	return c
+	server.ConnectServer(path, h, port)
 }

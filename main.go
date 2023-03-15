534ba81ee32a99caf1531285f90da7c82ec0f0ce
el-zacharoo <you@example.com>
2023-03-15 14:21:46 +1300

    updated deps

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	// "github.com/endo-checker/common/store"
-
 	"github.com/endo-checker/common/server"
+
 	"github.com/endo-checker/patient/handler"
 	pbcnn "github.com/endo-checker/patient/internal/gen/patient/v1/patientv1connect"
 	"github.com/endo-checker/patient/store"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	port := "localhost:8080"
-	godotenv.Load()
-	// uri := os.Getenv("mongouri")
 
 	s := store.Connect()
 	svc := &handler.PatientServer{
